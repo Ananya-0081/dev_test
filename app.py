68d2f2d0c81177215e07e465200f3497b62a1674
Ananya Sharma <as7567112@gmail.com>
2026-06-15 18:28:31 +0000

    firat commit through replit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World its my first flask app"
+    return "Hello World this is my first flask app"
 
 
 if __name__ == "__main__":
